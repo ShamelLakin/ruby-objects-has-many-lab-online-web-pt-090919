fa0b1c36a1589e7e167cf8e12fa4303558104ddd
Shamel Lakin <christlife4u@gmail.com>
2019-10-09 15:48:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,8 +9,8 @@ class Author
   end
   
   def posts
-        Post.all.select do |posts|
-          posts.author == self
+        Post.all.select do |post|
+          post.author == self
         end
     end
   
