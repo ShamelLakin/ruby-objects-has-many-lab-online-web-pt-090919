62b18c174eec4acd1b3ed67a21140b5502865f58
Shamel Lakin <christlife4u@gmail.com>
2019-10-09 15:10:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,25 +8,25 @@ class Author
     @posts = []
   end
 
-  def posts
-    @posts
-  end
+  # def posts
+  #   @posts
+  # end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
+  # def add_post(post)
+  #   @posts << post
+  #   post.author = self
+  #   @@post_count += 1
+  # end
 
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
+  # def add_post_by_title(post_title)
+  #   post = Post.new(post_title)
+  #   @posts << post
+  #   post.author = self
+  #   @@post_count += 1
+  # end
 
-  def self.post_count
-    @@post_count
-  end
+  # def self.post_count
+  #   @@post_count
+  # end
 
 end 
