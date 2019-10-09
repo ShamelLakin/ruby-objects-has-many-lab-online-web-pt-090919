6d6d7e40c75dfd7b38738635c8baa2fbc090f029
Shamel Lakin <christlife4u@gmail.com>
2019-10-09 16:12:22 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,39 @@
+class Author 
+  
+  attr_accessor :name, :posts
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+        Post.all.select do |posts|
+          posts.author == self
+        end
+    end
+  
+
+  def add_post(post)
+    post.author = self
+    # @posts << post
+    # post.author = self
+    # @@post_count += 1
+  end
+
+  def add_post_by_title(post_title)
+    singer = Post.new(post_title)
+      add_post(singer)
+    # post = Post.new(post_title)
+    # @posts << post
+    # post.author = self
+    # @@post_count += 1
+  end
+
+  def self.post_count
+     Post.all.count
+    # @@post_count.count.self
+  end
+
+end 
